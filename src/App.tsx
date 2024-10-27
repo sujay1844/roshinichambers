c230201a8cbb000ef0c4fed3ed87507ce2c8ef24
Sujay R <68642484+sujay1844@users.noreply.github.com>
2024-10-27 14:30:38 +0530

    Add placeholders for cards and reveal text

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,129 @@
-export default function App() {
+import { motion, useTransform, useScroll } from "framer-motion";
+import { useRef } from "react";
+
+const Example = () => {
   return (
-    <div className="w-full h-screen flex flex-col items-center justify-center ">
-      <h1>roshinichambers</h1>
+    <div className="bg-neutral-800">
+      <div className="flex h-screen items-center justify-center">
+        <span className="font-semibold uppercase text-neutral-500">
+          Welcome to Roshini Chambers
+        </span>
+        <p className="absolute bottom-10 text-white">Scroll down</p>
+      </div>
+      <HorizontalScrollCarousel />
     </div>
   );
-}
+};
+
+const HorizontalScrollCarousel = () => {
+  const targetRef = useRef<HTMLDivElement | null>(null);
+  const { scrollYProgress } = useScroll({
+    target: targetRef,
+  });
+
+  const x = useTransform(scrollYProgress, [0, 0.9], ["1%", "-100%"]);
+  const x2 = useTransform(scrollYProgress, [0, 0.9], ["-101%", "1%"]);
+  const opacity = useTransform(scrollYProgress, [0.7, 1], ["0", "1"]);
+
+  return (
+    <section ref={targetRef} className="relative h-[600vh] bg-neutral-900">
+      <div className="sticky top-0 flex flex-col items-start gap-4 h-screen overflow-hidden">
+        <div className="absolute top-0 left-0 w-full h-screen flex flex-col justify-center items-center">
+          <motion.div
+            style={{
+              opacity,
+            }}
+          >
+            <motion.div className="text-6xl text-blue-600 bg-red-500 w-full h-full">
+              Hello
+            </motion.div>
+          </motion.div>
+        </div>
+        <motion.div style={{ x }} className="flex gap-4">
+          {cards.map((card) => {
+            return <Card card={card} key={card.id} />;
+          })}
+        </motion.div>
+        <motion.div style={{ x: x2 }} className="flex gap-4 ml-[100vw]">
+          {cards.map((card) => {
+            return <Card card={card} key={card.id} />;
+          })}
+        </motion.div>
+        <motion.div style={{ x }} className="flex gap-4">
+          {cards.map((card) => {
+            return <Card card={card} key={card.id} />;
+          })}
+        </motion.div>
+      </div>
+    </section>
+  );
+};
+
+const Card = ({ card }: { card: CardType }) => {
+  return (
+    <div
+      key={card.id}
+      className="group relative h-[300px] w-[450px] overflow-hidden bg-neutral-200"
+    >
+      <div
+        style={{
+          backgroundImage: `url(${card.url})`,
+          backgroundSize: "cover",
+          backgroundPosition: "center",
+        }}
+        className="absolute inset-0 z-0 transition-transform duration-300 group-hover:scale-110"
+      ></div>
+      <div className="absolute inset-0 z-10 grid place-content-center">
+        <p className="bg-gradient-to-br from-white/20 to-white/0 p-8 text-6xl font-black uppercase text-white backdrop-blur-lg">
+          {card.title}
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default Example;
+
+type CardType = {
+  url: string;
+  title: string;
+  id: number;
+};
+
+const cards: CardType[] = [
+  {
+    url: "/imgs/abstract/1.jpg",
+    title: "Title 1",
+    id: 1,
+  },
+  {
+    url: "/imgs/abstract/2.jpg",
+    title: "Title 2",
+    id: 2,
+  },
+  {
+    url: "/imgs/abstract/3.jpg",
+    title: "Title 3",
+    id: 3,
+  },
+  {
+    url: "/imgs/abstract/4.jpg",
+    title: "Title 4",
+    id: 4,
+  },
+  {
+    url: "/imgs/abstract/5.jpg",
+    title: "Title 5",
+    id: 5,
+  },
+  {
+    url: "/imgs/abstract/6.jpg",
+    title: "Title 6",
+    id: 6,
+  },
+  {
+    url: "/imgs/abstract/7.jpg",
+    title: "Title 7",
+    id: 7,
+  },
+];
